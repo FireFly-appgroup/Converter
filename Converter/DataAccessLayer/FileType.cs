30e08d64635b6baa7a94cf63a88c9a0efefbbb15
Petr Lozhchenko <firefly.app.group@gmail.com>
2018-03-18 22:13:27 +0200

    Adding Inrefaces and Structures to DAL

---
+++ Converter/DataAccessLayer/FileType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.DataAccessLayer
+{
+    enum FileType
+    {
+        CSV = 0, txt = 1, pdf = 2, doc = 4, bin = 8
+    }
+}
---
+++ Converter/DataAccessLayer/Structures/File.cs
@@ -0,0 +1,26 @@
+﻿using Converter.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.DataAccessLayer.Structures
+{
+    public class File : IFile
+    {
+        public double Size { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<TradeRecord> tradeRecord = new List<TradeRecord>();
+
+        public void ToBinary()
+        {
+
+        }
+        public void Save()
+        {
+
+        }
+    }
+}
